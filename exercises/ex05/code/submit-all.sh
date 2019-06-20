5ab6c8014ac873e6a86109e8258321be710d0cb0
Ag Stephens <ag.stephens@stfc.ac.uk>
2019-06-20 15:22:23 +0100

    Updated again

--- exercises/ex05/code/submit-all.sh
+++ exercises/ex05/code/submit-all.sh
@@ -14,6 +14,6 @@ mkdir -p $OUTPUTS_DIR
 for i in $(seq 0 29); do
     day=$(date -d "2018-01-01 $i days" +%Y%m%d)
     echo "[INFO] Submitting job to LOTUS for date: $day"
-    bsub -q short-serial -W 00:05 -o $OUTPUTS_DIR/${day}.out -e $OUTPUTS_DIR/${day}.err $EXTRACTOR $day 
+    bsub -q workshop -W 00:05 -o $OUTPUTS_DIR/${day}.%J.out -e $OUTPUTS_DIR/${day}.%J.err $EXTRACTOR $day 
 done
 
