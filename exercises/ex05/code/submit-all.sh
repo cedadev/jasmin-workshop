bdfc5907b705c1094c7439547669298bc9f85ad7
Ag Stephens <ag.stephens@stfc.ac.uk>
2019-06-20 15:24:37 +0100

    Updated script

--- exercises/ex05/code/submit-all.sh
+++ exercises/ex05/code/submit-all.sh
@@ -9,11 +9,12 @@ EXTRACTOR=exercises/ex5/extract-era-data.sh
 OUTPUTS_DIR=exercises/users/$USER/ex5/lotus-outputs
 
 mkdir -p $OUTPUTS_DIR
+queue=workshop
 
 
 for i in $(seq 0 29); do
     day=$(date -d "2018-01-01 $i days" +%Y%m%d)
     echo "[INFO] Submitting job to LOTUS for date: $day"
-    bsub -q workshop -W 00:05 -o $OUTPUTS_DIR/${day}.%J.out -e $OUTPUTS_DIR/${day}.%J.err $EXTRACTOR $day 
+    bsub -q $queue -W 00:05 -o $OUTPUTS_DIR/${day}.%J.out -e $OUTPUTS_DIR/${day}.%J.err $EXTRACTOR $day 
 done
 
