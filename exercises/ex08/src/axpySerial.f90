b19d2af88a3a73e8f5825f68e461571708a9df82
Fatima Chami <fchami@jasmin-sci1.ceda.ac.uk>
2019-06-25 21:16:50 +0100

    Added exercise 8 source code

---
+++ exercises/ex08/src/axpySerial.f90
@@ -0,0 +1,41 @@
+subroutine axpy(n, a, X, Y, Z)
+use iso_fortran_env, only: real64
+implicit none
+integer :: n
+real(real64) :: a
+real(real64) :: X(*), Y(*), Z(*)
+integer :: i
+
+  do i = 1, n
+    Z(i) = a * X(i) + Y(i)
+  end do
+
+end subroutine axpy
+
+program axpyProg
+use iso_fortran_env, only: real64
+implicit none
+real(real64) :: a
+real(real64), allocatable :: X(:), Y(:), Z(:)
+integer :: n
+
+  n = 2**10
+
+  allocate(X(n))
+  allocate(Y(n))
+  allocate(Z(n))
+
+  call random_seed
+  call random_number(a)
+  call random_number(X)
+  call random_number(Y)
+
+  call axpy(n, a, X, Y, Z)
+
+  print *, maxval(abs(Z)), maxval(abs(Z / (a * X + Y) - 1.0_real64))
+
+  deallocate(X)
+  deallocate(Y)
+  deallocate(Z)
+
+end program axpyProg
