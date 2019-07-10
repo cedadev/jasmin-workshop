7660e87f678fe66df48efa205ccd4147bb2c2b80
agstephens <ag.stephens@stfc.ac.uk>
2019-07-10 15:55:15 +0100

    Updated code in ex05

--- exercises/ex05/code/submit-all.sh
+++ exercises/ex05/code/submit-all.sh
@@ -13,8 +13,10 @@ queue=workshop
 
 
 for i in $(seq 0 1); do
+
     day=$(date -d "2018-01-01 $i days" +%Y%m%d)
     echo "[INFO] Submitting job to LOTUS for date: $day"
     bsub -q $queue -W 00:05 -o $OUTPUTS_DIR/${day}.%J.out -e $OUTPUTS_DIR/${day}.%J.err $EXTRACTOR $day 
+
 done
 
