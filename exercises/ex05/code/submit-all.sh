a7751d0f98e17fa12db590692af4ac158af7e4b8
Ag Stephens <ag.stephens@stfc.ac.uk>
2020-01-29 12:58:28 +0000

    Updated: submit-all.sh - queue and estimate
    Changed queue to "short-serial".
    Changed bsub to add argument to estimate the runtime to 1 minute.

--- exercises/ex05/code/submit-all.sh
+++ exercises/ex05/code/submit-all.sh
@@ -9,14 +9,14 @@ EXTRACTOR=$PWD/extract-era-data.sh
 OUTPUTS_DIR=/group_workspaces/jasmin2/workshop/users/$USER/ex05/lotus-outputs
 
 mkdir -p $OUTPUTS_DIR
-queue=workshop
+queue=short-serial
 
 
 for i in $(seq 0 1); do
 
     day=$(date -d "2018-01-01 $i days" +%Y%m%d)
     echo "[INFO] Submitting job to LOTUS for date: $day"
-    bsub -q $queue -W 00:05 -o $OUTPUTS_DIR/${day}.%J.out -e $OUTPUTS_DIR/${day}.%J.err $EXTRACTOR $day 
+    bsub -q $queue -W 00:05 -We 00:01 -o $OUTPUTS_DIR/${day}.%J.out -e $OUTPUTS_DIR/${day}.%J.err $EXTRACTOR $day 
 
 done
 
